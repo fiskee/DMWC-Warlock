4293e9a0336a718a038c024f86933d0d35350eed
Fiskee <fisker@gmail.com>
2019-08-26 22:34:32 +0200

    Basic init

--- Rotation.lua
+++ Rotation.lua
@@ -1,1 +1,22 @@
 local DMW = DMW
+local Warlock = DMW.Rotations.WARLOCK
+local Rotation = DMW.Helpers.Rotation
+local Setting = DMW.Helpers.Rotation.Setting
+local Player, Buff, Debuff, Spell, Target, Talent, Item, GCD, CDs, HUD, Enemy40Y, Enemy40YC
+
+local function Locals()
+    Player = DMW.Player
+    Buff = Player.Buffs
+    Debuff = Player.Debuffs
+    Spell = Player.Spells
+    Talent = Player.Talents
+    Item = Player.Items
+    Target = Player.Target or false
+    HUD = DMW.Settings.profile.HUD
+    CDs = Player:CDs()
+    Enemy40Y, Enemy40YC = Player:GetEnemies(40)
+end
+
+function Warlock.Rotation()
+    Locals()
+end
--- Settings.lua
+++ Settings.lua
@@ -1,1 +1,21 @@
 local DMW = DMW
+DMW.Rotations.WARLOCK = {}
+local Warlock = DMW.Rotations.WARLOCK
+local UI = DMW.UI
+
+function Warlock.Settings()
+    UI.HUD.Options = {
+        [1] = {
+            Test = {
+                [1] = {Text = "HUD Test |cFF00FF00On", Tooltip = ""},
+                [2] = {Text = "HUD Test |cFFFFFF00Sort Of On", Tooltip = ""},
+                [3] = {Text = "HUD Test |cffff0000Disabled", Tooltip = ""}
+            }
+        }
+    }
+
+    UI.AddHeader("This Is A Header")
+    UI.AddDropdown("This Is A Dropdown", nil, {"Yay", "Nay"}, 1)
+    UI.AddToggle("This Is A Toggle", "This is a tooltip", true)
+    UI.AddRange("This Is A Range", "One more tooltip", 0, 100, 1, 70)
+end
