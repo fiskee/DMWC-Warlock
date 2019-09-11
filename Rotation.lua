6f7c179204e0cdd9caf1edd59eb5e8972541822c
Fiskee <fisker@gmail.com>
2019-09-11 09:17:48 +0200

    Interrupt casts/channels to drain soul snipe

--- Rotation.lua
+++ Rotation.lua
@@ -115,143 +115,151 @@ end
 
 function Warlock.Rotation()
     Locals()
-    if not Player.Combat and not Player.Moving and (not Pet or Pet.Dead) and Setting("Pet") ~= 1 then
-        if Setting("Pet") == 2 and not Spell.SummonImp:LastCast() and Spell.SummonImp:Cast(Player) then
-            return true
-        elseif Setting("Pet") == 3 and not Spell.SummonVoidwalker:LastCast() and Spell.SummonVoidwalker:Cast(Player) then
-            return true
-        elseif Setting("Pet") == 4 and not Spell.SummonSuccubus:LastCast() and Spell.SummonSuccubus:Cast(Player) then
-            return true
-        elseif Setting("Pet") == 5 and not Spell.SummonFelhunter:LastCast() and Spell.SummonFelhunter:Cast(Player) then
-            return true
+    if not Player.Casting then
+        if not Player.Combat and not Player.Moving and (not Pet or Pet.Dead) and Setting("Pet") ~= 1 then
+            if Setting("Pet") == 2 and not Spell.SummonImp:LastCast() and Spell.SummonImp:Cast(Player) then
+                return true
+            elseif Setting("Pet") == 3 and not Spell.SummonVoidwalker:LastCast() and Spell.SummonVoidwalker:Cast(Player) then
+                return true
+            elseif Setting("Pet") == 4 and not Spell.SummonSuccubus:LastCast() and Spell.SummonSuccubus:Cast(Player) then
+                return true
+            elseif Setting("Pet") == 5 and not Spell.SummonFelhunter:LastCast() and Spell.SummonFelhunter:Cast(Player) then
+                return true
+            end
         end
-    end
-    if Setting("Auto Delete Shards") then
-        DeleteShards(Setting("Max Shards"))
-    end
-    if Setting("Auto Target Quest Units") then
-        if Player:AutoTargetQuest(30, true) then
-            return true
+        if Setting("Auto Delete Shards") then
+            DeleteShards(Setting("Max Shards"))
         end
-    end
-    if Player.Combat and Setting("Auto Target") then
-        if Player:AutoTarget(30, true) then
-            return true
+        if Setting("Auto Target Quest Units") then
+            if Player:AutoTargetQuest(30, true) then
+                return true
+            end
         end
-    end
-    if not Player.Combat then
-        if Setting("Auto Buff") and DemonBuff() then
-            return true
+        if Player.Combat and Setting("Auto Target") then
+            if Player:AutoTarget(30, true) then
+                return true
+            end
         end
-        if not Player.Moving and Setting("Create Healthstone") and CreateHealthstone() then
-            return true
-        end
-        if Setting("Life Tap OOC") and Setting("Life Tap") and Player.HP >= Setting("Life Tap HP") and Player.PowerPct <= Setting("Life Tap Mana") and Spell.LifeTap:Cast(Player) then
-            return true
+        if not Player.Combat then
+            if Setting("Auto Buff") and DemonBuff() then
+                return true
+            end
+            if not Player.Moving and Setting("Create Healthstone") and CreateHealthstone() then
+                return true
+            end
+            if Setting("Life Tap OOC") and Setting("Life Tap") and Player.HP >= Setting("Life Tap HP") and Player.PowerPct <= Setting("Life Tap Mana") and Spell.LifeTap:Cast(Player) then
+                return true
+            end
         end
     end
     if Target and Target.ValidEnemy and Target.Distance < 40 then
-        if Defensive() then
-            return true
-        end
-        --Force refresh on fear
-        if Setting("Corruption") and Debuff.Fear:Exist(Target) and Spell.Fear:LastCast() and Debuff.Corruption:Remain(Target) < 5 and (not Player.Moving or Talent.ImprovedCorruption.Rank == 5) and Spell.Corruption:Cast(Target) then
-            return true
+        if not Player.Casting then
+            if Defensive() then
+                return true
+            end
+            --Force refresh on fear
+            if Setting("Corruption") and Debuff.Fear:Exist(Target) and Spell.Fear:LastCast() and Debuff.Corruption:Remain(Target) < 5 and (not Player.Moving or Talent.ImprovedCorruption.Rank == 5) and Spell.Corruption:Cast(Target) then
+                return true
+            end
         end
         if not Player.Moving and Setting("Drain Soul Snipe") then
             for _, Unit in ipairs(Enemy30Y) do
-                if Unit.Facing and (Unit.TTD < 3 or Unit.HP < 8) and not Unit:IsBoss() and not UnitIsTapDenied(Unit.Pointer) and Spell.DrainSoul:Cast(Unit) then
-                    WandTime = DMW.Time
-                    return true
+                if Unit.Facing and (Unit.TTD < 3 or Unit.HP < 8) and not Unit:IsBoss() and not UnitIsTapDenied(Unit.Pointer) and Spell.DrainSoul:CD() < 0.2 then
+                    if (not Player.Casting or Player.Casting ~= Spell.DrainSoul.SpellName) and Spell.DrainSoul:Cast(Unit) then
+                        WandTime = DMW.Time
+                        return true
+                    end
                 end
             end
         end
-        if not Player.Moving and Setting("Fear Bonus Mobs") and Debuff.Fear:Count() == 0 and (not Spell.Fear:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7)) then
-            if Enemy20YC > 1 and not Player.InGroup then
-                local CreatureType
-                for i, Unit in ipairs(Enemy20Y) do
-                    if i > 1 then
-                        CreatureType = Unit.CreatureType
-                        if Unit.TTD > 3 and not (CreatureType == "Undead" or CreatureType == "Mechanical" or CreatureType == "Totem") and not Unit:IsBoss() and Spell.Fear:Cast(Unit) then
+        if not Player.Casting then
+            if not Player.Moving and Setting("Fear Bonus Mobs") and Debuff.Fear:Count() == 0 and (not Spell.Fear:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7)) then
+                if Enemy20YC > 1 and not Player.InGroup then
+                    local CreatureType
+                    for i, Unit in ipairs(Enemy20Y) do
+                        if i > 1 then
+                            CreatureType = Unit.CreatureType
+                            if Unit.TTD > 3 and not (CreatureType == "Undead" or CreatureType == "Mechanical" or CreatureType == "Totem") and not Unit:IsBoss() and Spell.Fear:Cast(Unit) then
+                                return true
+                            end
+                        end
+                    end
+                end
+            end
+            if Setting("Auto Pet Attack") and Pet and not Pet.Dead and not UnitIsUnit(Target.Pointer, "pettarget") and DMW.Time > (PetAttackTime + 1) then
+                PetAttackTime = DMW.Time
+                PetAttack()
+            end
+            if (not DMW.Player.Equipment[18] or (Target.Distance <= 1 and Setting("Auto Attack In Melee"))) and not IsCurrentSpell(Spell.Attack.SpellID) then
+                StartAttack()
+            end
+            --Corruption
+            if Setting("Corruption") and (not Player.Moving or Talent.ImprovedCorruption.Rank == 5) then
+                if (not Spell.Corruption:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Corruption.LastBotTarget, Target.Pointer)) and Target.CreatureType ~= "Totem" and Target.Facing and not Debuff.Corruption:Exist(Target) and Target.TTD > 7 and Spell.Corruption:Cast(Target) then
+                    return true
+                end
+                if Setting("Cycle Corruption") then
+                    for _, Unit in ipairs(Enemy30Y) do
+                        if (not Spell.Corruption:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Corruption.LastBotTarget, Unit.Pointer)) and Unit.CreatureType ~= "Totem" and Unit.Facing and not Debuff.Corruption:Exist(Unit) and Unit.TTD > 7 and Spell.Corruption:Cast(Unit) then
                             return true
                         end
                     end
                 end
             end
-        end
-        if Setting("Auto Pet Attack") and Pet and not Pet.Dead and not UnitIsUnit(Target.Pointer, "pettarget") and DMW.Time > (PetAttackTime + 1) then
-            PetAttackTime = DMW.Time
-            PetAttack()
-        end
-        if (not DMW.Player.Equipment[18] or (Target.Distance <= 1 and Setting("Auto Attack In Melee"))) and not IsCurrentSpell(Spell.Attack.SpellID) then
-            StartAttack()
-        end
-        --Corruption
-        if Setting("Corruption") and (not Player.Moving or Talent.ImprovedCorruption.Rank == 5) then
-            if (not Spell.Corruption:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Corruption.LastBotTarget, Target.Pointer)) and Target.CreatureType ~= "Totem" and Target.Facing and not Debuff.Corruption:Exist(Target) and Target.TTD > 7 and Spell.Corruption:Cast(Target) then
-                return true
-            end
-            if Setting("Cycle Corruption") then
-                for _, Unit in ipairs(Enemy30Y) do
-                    if (not Spell.Corruption:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Corruption.LastBotTarget, Unit.Pointer)) and Unit.CreatureType ~= "Totem" and Unit.Facing and not Debuff.Corruption:Exist(Unit) and Unit.TTD > 7 and Spell.Corruption:Cast(Unit) then
-                        return true
+            --SL
+            if Setting("Siphon Life") then
+                if not Debuff.SiphonLife:Exist(Target) and Target.TTD > 10 and Target.CreatureType ~= "Totem" and Spell.SiphonLife:Cast(Target) then
+                    return true
+                end
+                if Setting("Cycle Siphon Life") then
+                    for _, Unit in ipairs(Enemy30Y) do
+                        if not Debuff.SiphonLife:Exist(Unit) and Unit.TTD > 10 and Unit.CreatureType ~= "Totem" and Spell.SiphonLife:Cast(Unit) then
+                            return true
+                        end
                     end
                 end
             end
-        end
-        --SL
-        if Setting("Siphon Life") then
-            if not Debuff.SiphonLife:Exist(Target) and Target.TTD > 10 and Target.CreatureType ~= "Totem" and Spell.SiphonLife:Cast(Target) then
-                return true
-            end
-            if Setting("Cycle Siphon Life") then
-                for _, Unit in ipairs(Enemy30Y) do
-                    if not Debuff.SiphonLife:Exist(Unit) and Unit.TTD > 10 and Unit.CreatureType ~= "Totem" and Spell.SiphonLife:Cast(Unit) then
-                        return true
+            --CoA
+            if Setting("Curse of Agony") then
+                if not Debuff.CurseOfAgony:Exist(Target) and Target.TTD > 10 and Target.CreatureType ~= "Totem" and Spell.CurseOfAgony:Cast(Target) then
+                    return true
+                end
+                if Setting("Cycle Curse of Agony") then
+                    for _, Unit in ipairs(Enemy30Y) do
+                        if not Debuff.CurseOfAgony:Exist(Unit) and Unit.TTD > 10 and Unit.CreatureType ~= "Totem" and Spell.CurseOfAgony:Cast(Unit) then
+                            return true
+                        end
                     end
                 end
             end
-        end
-        --CoA
-        if Setting("Curse of Agony") then
-            if not Debuff.CurseOfAgony:Exist(Target) and Target.TTD > 10 and Target.CreatureType ~= "Totem" and Spell.CurseOfAgony:Cast(Target) then
-                return true
-            end
-            if Setting("Cycle Curse of Agony") then
-                for _, Unit in ipairs(Enemy30Y) do
-                    if not Debuff.CurseOfAgony:Exist(Unit) and Unit.TTD > 10 and Unit.CreatureType ~= "Totem" and Spell.CurseOfAgony:Cast(Unit) then
-                        return true
+            --Immolate
+            if Setting("Immolate") and not Player.Moving then
+                if (not Spell.Immolate:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Immolate.LastBotTarget, Target.Pointer)) and Target.CreatureType ~= "Totem" and Target.Facing and not Debuff.Immolate:Exist(Target) and Target.TTD > 10 and Spell.Immolate:Cast(Target) then
+                    return true
+                end
+                if Setting("Cycle Immolate") then
+                    for _, Unit in ipairs(Enemy30Y) do
+                        if (not Spell.Immolate:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Immolate.LastBotTarget, Unit.Pointer)) and Unit.CreatureType ~= "Totem" and Unit.Facing and not Debuff.Immolate:Exist(Unit) and Unit.TTD > 10 and Spell.Immolate:Cast(Unit) then
+                            return true
+                        end
                     end
                 end
             end
-        end
-        --Immolate
-        if Setting("Immolate") and not Player.Moving then
-            if (not Spell.Immolate:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Immolate.LastBotTarget, Target.Pointer)) and Target.CreatureType ~= "Totem" and Target.Facing and not Debuff.Immolate:Exist(Target) and Target.TTD > 10 and Spell.Immolate:Cast(Target) then
+            if Setting("Life Tap") and Player.HP >= Setting("Life Tap HP") and Player.PowerPct <= Setting("Life Tap Mana") and Spell.LifeTap:Cast(Player) then
                 return true
             end
-            if Setting("Cycle Immolate") then
-                for _, Unit in ipairs(Enemy30Y) do
-                    if (not Spell.Immolate:LastCast() or (DMW.Player.LastCast[1].SuccessTime and (DMW.Time - DMW.Player.LastCast[1].SuccessTime) > 0.7) or not UnitIsUnit(Spell.Immolate.LastBotTarget, Unit.Pointer)) and Unit.CreatureType ~= "Totem" and Unit.Facing and not Debuff.Immolate:Exist(Unit) and Unit.TTD > 10 and Spell.Immolate:Cast(Unit) then
-                        return true
-                    end
-                end
+            if Setting("Shadow Bolt Mode") == 2 and Target.Facing and not Player.Moving and Player.PowerPct > Setting("Shadow Bolt Mana") and (Target.TTD > Spell.ShadowBolt:CastTime() or (Target.Distance > 5 and not DMW.Player.Equipment[18])) and Spell.ShadowBolt:Cast(Target) then
+                return true
             end
-        end
-        if Setting("Life Tap") and Player.HP >= Setting("Life Tap HP") and Player.PowerPct <= Setting("Life Tap Mana") and Spell.LifeTap:Cast(Player) then
-            return true
-        end
-        if Setting("Shadow Bolt Mode") == 2 and Target.Facing and not Player.Moving and Player.PowerPct > Setting("Shadow Bolt Mana") and (Target.TTD > Spell.ShadowBolt:CastTime() or (Target.Distance > 5 and not DMW.Player.Equipment[18])) and Spell.ShadowBolt:Cast(Target) then
-            return true
-        end
-        if Setting("Shadow Bolt Mode") == 3 and Target.Facing and Player.PowerPct > Setting("Shadow Bolt Mana") and Buff.ShadowTrance:Exist(Player) and Spell.ShadowBolt:Cast(Target) then
-            return true
-        end
-        if Setting("Drain Life Filler") and Player.HP <= Setting("Drain Life Filler HP") and Target.CreatureType ~= "Mechanical" and Target.TTD > 3 and Spell.DrainLife:Cast(Target) then
-            return true
-        end
-        if DMW.Player.Equipment[18] and Target.Facing and Wand() then
-            return true
+            if Setting("Shadow Bolt Mode") == 3 and Target.Facing and Player.PowerPct > Setting("Shadow Bolt Mana") and Buff.ShadowTrance:Exist(Player) and Spell.ShadowBolt:Cast(Target) then
+                return true
+            end
+            if Setting("Drain Life Filler") and Player.HP <= Setting("Drain Life Filler HP") and Target.CreatureType ~= "Mechanical" and Target.TTD > 3 and Spell.DrainLife:Cast(Target) then
+                return true
+            end
+            if DMW.Player.Equipment[18] and Target.Facing and Wand() then
+                return true
+            end
         end
     end
 end
--- Settings.lua
+++ Settings.lua
@@ -50,4 +50,6 @@ function Warlock.Settings()
     UI.AddToggle("Drain Soul Snipe", "Try to auto snipe enemies with drain soul, useful for shard farming or Improved Drain Soul talent", false)
     UI.AddToggle("Auto Delete Shards", "Activate automatic deletion of shards from bags, set max below", false)
     UI.AddRange("Max Shards", "Control max number of shards in bag", 0, 20, 1, 4)
+    --
+    DMW.Helpers.Rotation.CastingCheck = false
 end
